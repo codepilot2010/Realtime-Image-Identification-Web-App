5b583f8de52e000750a66d39a81283870763caaa
codepilot2010 <93812156+codepilot2010@users.noreply.github.com>
2023-04-16 19:37:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,38 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  classifier=ml5.imageClassifier('MobileNet',modeLoaded);
+}
+
+function modeLoaded(){
+  console.log('model loaded');
+}
+
+function draw(){
+  image(video,0,0,300,300);
+   classifier.classify(video,gotResult);
+}
+
+var previousresult="";
+ 
+function gotResult(error,results){
+  if(error)
+  {
+    console.error(error);
+  }
+  else{
+    if((results[0].confidence>0.5) && (previousresult!=results[0].label))
+    {
+      console.log(results);
+      previousresult=results[0].label;
+      var synth=window.speechSynthesis;
+      speakdata="object detected is - "+results[0].label;
+      var utterthis=new SpeechSynthesisUtterance(speakdata);
+      synth.speak(utterthis);
+      document.getElementById("result_object_name").innerHTML=results[0].label;
+      document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+  }
 }
 
 
